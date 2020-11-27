2222fc204ec90982b0f7edfdd91f07a960dfe53b
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2020-11-27 10:02:07 +0100

    data

--- src/samiha.js
+++ src/samiha.js
@@ -1,59 +1,8 @@
 import $ from 'jquery';
+import { myQuestions } from './data';
 
 const crossing = document.getElementById('croisement');
 const quiz = document.getElementById('quiz');
-const myQuestions = [
-  /*
-  {
-    question: 'Brendan Eich invented JavaScript?',
-    answers: {
-      a: 'true',
-      b: 'false',
-    },
-    correctAnswer: 'true',
-  },
-  {
-    question: 'Node.js is a JavaScript package manager?',
-    answers: {
-      a: 'true',
-      b: 'false',
-    },
-    correctAnswer: 'false',
-  },
-  {
-    question: ' To ensure code quality can you use the tool jQuery?',
-    answers: {
-      a: 'true',
-      b: 'false',
-    },
-    correctAnswer: 'false',
-  },
-  {
-    question: 'The <body> section is the correct place to insert a JavaScript?',
-    answers: {
-      a: 'true',
-      b: 'false',
-    },
-    correctAnswer: 'false',
-  },
-  */
-  {
-    question: 'Un flamant rose pèse quatre kilogrammes quand il se tient debout sur deux pattes. Combien de kilogrammes pèsera-t-il s\'il lève une patte ?',
-    answers: {
-      a: '2kg',
-      b: '4kg',
-    },
-    correctAnswer: '4kg',
-  },
-  {
-    question: 'Tu participes à une course cycliste. A un moment donné, tu doubles le deuxième. Tu deviens...',
-    answers: {
-      a: 'Premier',
-      b: 'Deuxième',
-    },
-    correctAnswer: 'Deuxième',
-  },
-];
 
 export function buildQuiz() {
   crossing.disabled = true;
